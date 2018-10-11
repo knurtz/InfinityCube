727e06a5f7875fc3b1075ecddf5cbc44b7e6c600
Johannes Herold <johannes_herold@turag.de>
2018-10-11 16:21:13 +0200

    Grundstruktur der main.c festgelegt, Klassen geplant

--- src/main.c
+++ src/main.c
@@ -12,9 +12,38 @@
 #include "ws2812.h"
 #include "delay.h"
 
+// neue Klasse: LEDCube
+// kriegt LEDBuffer, LEDs, Offset und Richtung pro Kante
+// Funktionen:
+// setPixel(side, number, colorRGB);
+// setSide(side, colorRGB);
+// setAll(colorRGB);
+
+// neue Klasse: SimpleAnimation
+// kriegt LEDCube
+// Funktionen:
+// nextFrame(); -> geht alle Seiten des Würfels durch und setzt der Reihe nach einen Pixel
+
+// weitere Animationsklassen
+
 
 void main(void) {
 
+	// initialize everything
 	delay_init (DELAY_RESOLUTION_5_US);				// init delay with granularity of 5us
 	ws2812_init();
+
+	while(1) {
+		// Main Loop Delay für 40 fps
+
+
+		// von aktueller Animationsklasse nextFrame() aufrufen
+
+
+		// LEDs refreshen, falls WS2812 nicht im kontinuierlichen Modus läuft
+
+
+		// nach einiger Zeit: Animationsklasse abwechseln
+
+	}
 }
